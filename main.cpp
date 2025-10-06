8cb995c5e4900076314bcccd13b39428a3d845ab
guiviana <guiguiviana70@gmail.com>
2025-10-06 00:55:16 -0300

    Organização e readme

--- main.cpp
+++ main.cpp
@@ -24,10 +24,13 @@ bool is_digraph(std::istream& is) {
 }
 
 int main() {
-    
+
     std::string caminho;
     std::cout << "Insira o nome do arquivo a ser lido: ";
     std::cin >> caminho;
+
+    bool whatsearch = 0;
+    //0 para dfs, 1 para bfs
 
     std::ifstream in(caminho);
     if (!in) { std::cerr << caminho << " não existe\n"; return 1; }
@@ -37,144 +40,156 @@ int main() {
     in.clear();
     in.seekg(0);
 
-    if (directed) {
-        DG g;
-        boost::dynamic_properties dp(boost::ignore_other_properties);
-        dp.property("node_id", get(&V::name, g));
-        dp.property("label",   get(&V::label, g));
-        dp.property("label",   get(&E::label, g));
-        if (!boost::read_graphviz(in, g, dp)) { std::cerr << "falha DOT\n"; return 2; }
-        std::cout << "Grafo dirigido lido.\n";
-        // for (auto e : boost::make_iterator_range(edges(g)))
-        //     std::cout << g[source(e,g)].name << " -> " << g[target(e,g)].name
-        //               << " label=" << g[e].label << "\n";
+    if (!whatsearch) { //AQUI SERÁ IMPLEMENTADA A DFS DE LUTERCIO -----------------------------------------------------------------------------------------------------------
 
-        std::map<std::string, std::set<std::string>> listaadj;
-        for (auto e : boost::make_iterator_range(edges(g))) {
-            const std::string& u = g[source(e, g)].name;
-            const std::string& v = g[target(e, g)].name;
-            listaadj[u].insert(v); 
-        }
+        if (directed) {
+            DG g;
+            boost::dynamic_properties dp(boost::ignore_other_properties);
+            dp.property("node_id", get(&V::name, g));
+            dp.property("label",   get(&V::label, g));
+            dp.property("label",   get(&E::label, g));
+            if (!boost::read_graphviz(in, g, dp)) { std::cerr << "falha DOT\n"; return 2; }
+            std::cout << "Grafo dirigido lido.\n";
+            // for (auto e : boost::make_iterator_range(edges(g)))
+            //     std::cout << g[source(e,g)].name << " -> " << g[target(e,g)].name
+            //               << " label=" << g[e].label << "\n";
 
-        // Inicio da BFS
-
-        auto index = get(boost::vertex_index, g);
-
-        std::vector<char> visitado(num_vertices(g), 0);
-        std::vector<int>  tin(num_vertices(g), -1);
-        std::vector<int>  tout(num_vertices(g), -1);
-        int tempo = 0;
-
-        for (auto v : boost::make_iterator_range(vertices(g))) {
-            if (visitado[index[v]]) {
-                continue;
+            std::map<std::string, std::set<std::string>> listaadj;
+            for (auto e : boost::make_iterator_range(edges(g))) {
+                const std::string& u = g[source(e, g)].name;
+                const std::string& v = g[target(e, g)].name;
+                listaadj[u].insert(v); 
             }
 
-            std::queue<UG::vertex_descriptor> fila;
+            // Inicio da BFS
 
-            visitado[index[v]] = 1;
-            tin[index[v]] = ++tempo;
-            fila.push(v);
+            auto index = get(boost::vertex_index, g);
 
-            std::cout << "Componente (BFS): ";
-            while (!fila.empty()) {
-                auto atual = fila.front();
-                fila.pop();
-                std::cout << g[atual].name << " ";
+            std::vector<char> visitado(num_vertices(g), 0);
+            std::vector<int>  tin(num_vertices(g), -1);
+            std::vector<int>  tout(num_vertices(g), -1);
+            int tempo = 0;
 
-                for (auto e : boost::make_iterator_range(out_edges(atual, g))) {
-                    auto w = target(e, g);
-                    if (!visitado[index[w]]) {
-                        visitado[index[w]] = 1;
-                        tin[index[w]] = ++tempo;
-                        fila.push(w);
-                    }
+            for (auto v : boost::make_iterator_range(vertices(g))) {
+                if (visitado[index[v]]) {
+                    continue;
                 }
 
-                tout[index[atual]] = ++tempo;
-            }
-            std::cout << "\n";
-        }
+                std::queue<UG::vertex_descriptor> fila;
 
-        std::cout << "\nTempos (descoberta/finalização):\n";
-        for (auto v : boost::make_iterator_range(vertices(g))) {
-            std::cout << std::left << std::setw(12) << g[v].name
-                    << " d=" << tin[index[v]] << " f=" << tout[index[v]] << "\n";
-        }
+                visitado[index[v]] = 1;
+                tin[index[v]] = ++tempo;
+                fila.push(v);
 
-    } else {
-        UG g;
-        boost::dynamic_properties dp(boost::ignore_other_properties);
-        dp.property("node_id", get(&V::name, g));
-        dp.property("label",   get(&V::label, g));
-        dp.property("label",   get(&E::label, g));
-        if (!boost::read_graphviz(in, g, dp)) { std::cerr << "falha DOT\n"; return 2; }
-        std::cout << "Grafo não dirigido lido.\n";
-        // for (auto e : boost::make_iterator_range(edges(g)))
-        //     std::cout << g[source(e,g)].name << " -- " << g[target(e,g)].name
-        //               << " label=" << g[e].label << "\n";
+                std::cout << "Componente (BFS): ";
+                while (!fila.empty()) {
+                    auto atual = fila.front();
+                    fila.pop();
+                    std::cout << g[atual].name << " ";
 
-        std::map<std::string, std::set<std::string>> listaadj;
-
-        for (auto e : boost::make_iterator_range(edges(g))) {
-            const std::string& u = g[source(e, g)].name;
-            const std::string& v = g[target(e, g)].name;
-            listaadj[u].insert(v); 
-            listaadj[v].insert(u);
-        }
-
-        for (const auto& [u, vizinhos] : listaadj) {
-            std::cout << u << ":";
-            for (const auto& v : vizinhos) {
-                std::cout << " " << v;
-            }
-            std::cout << "\n";
-        }
-
-        // Inicio da BFS
-
-        auto index = get(boost::vertex_index, g);
-
-        std::vector<char> visitado(num_vertices(g), 0);
-        std::vector<int>  tin(num_vertices(g), -1);
-        std::vector<int>  tout(num_vertices(g), -1);
-        int tempo = 0;
-
-        for (auto v : boost::make_iterator_range(vertices(g))) {
-            if (visitado[index[v]]) {
-                continue;
-            }
-
-            std::queue<UG::vertex_descriptor> fila;
-
-            visitado[index[v]] = 1;
-            tin[index[v]] = ++tempo;
-            fila.push(v);
-
-            std::cout << "Componente (BFS): ";
-            while (!fila.empty()) {
-                auto atual = fila.front();
-                fila.pop();
-                std::cout << g[atual].name << " ";
-
-                for (auto e : boost::make_iterator_range(out_edges(atual, g))) {
-                    auto w = target(e, g);
-                    if (!visitado[index[w]]) {
-                        visitado[index[w]] = 1;
-                        tin[index[w]] = ++tempo;
-                        fila.push(w);
+                    for (auto e : boost::make_iterator_range(out_edges(atual, g))) {
+                        auto w = target(e, g);
+                        if (!visitado[index[w]]) {
+                            visitado[index[w]] = 1;
+                            tin[index[w]] = ++tempo;
+                            fila.push(w);
+                        }
                     }
+
+                    tout[index[atual]] = ++tempo;
+                }
+                std::cout << "\n";
+            }
+
+            std::cout << "\nTempos (descoberta/finalização):\n";
+            for (auto v : boost::make_iterator_range(vertices(g))) {
+                std::cout << std::left << std::setw(12) << g[v].name
+                        << " d=" << tin[index[v]] << " f=" << tout[index[v]] << "\n";
+            }
+
+        } else {
+
+            UG g;
+            boost::dynamic_properties dp(boost::ignore_other_properties);
+            dp.property("node_id", get(&V::name, g));
+            dp.property("label",   get(&V::label, g));
+            dp.property("label",   get(&E::label, g));
+            if (!boost::read_graphviz(in, g, dp)) { std::cerr << "falha DOT\n"; return 2; }
+            std::cout << "Grafo não dirigido lido.\n";
+            // for (auto e : boost::make_iterator_range(edges(g)))
+            //     std::cout << g[source(e,g)].name << " -- " << g[target(e,g)].name
+            //               << " label=" << g[e].label << "\n";
+
+            std::map<std::string, std::set<std::string>> listaadj;
+
+            for (auto e : boost::make_iterator_range(edges(g))) {
+                const std::string& u = g[source(e, g)].name;
+                const std::string& v = g[target(e, g)].name;
+                listaadj[u].insert(v); 
+                listaadj[v].insert(u);
+            }
+
+            for (const auto& [u, vizinhos] : listaadj) {
+                std::cout << u << ":";
+                for (const auto& v : vizinhos) {
+                    std::cout << " " << v;
+                }
+                std::cout << "\n";
+            }
+
+            // Inicio da BFS
+
+            auto index = get(boost::vertex_index, g);
+
+            std::vector<char> visitado(num_vertices(g), 0);
+            std::vector<int>  tin(num_vertices(g), -1);
+            std::vector<int>  tout(num_vertices(g), -1);
+            int tempo = 0;
+
+            for (auto v : boost::make_iterator_range(vertices(g))) {
+                if (visitado[index[v]]) {
+                    continue;
                 }
 
-                tout[index[atual]] = ++tempo;
-            }
-            std::cout << "\n";
-        }
+                std::queue<UG::vertex_descriptor> fila;
 
-        std::cout << "\nTempos (descoberta/finalização):\n";
-        for (auto v : boost::make_iterator_range(vertices(g))) {
-            std::cout << std::left << std::setw(12) << g[v].name
-                    << " d=" << tin[index[v]] << " f=" << tout[index[v]] << "\n";
+                visitado[index[v]] = 1;
+                tin[index[v]] = ++tempo;
+                fila.push(v);
+
+                std::cout << "Componente (BFS): ";
+                while (!fila.empty()) {
+                    auto atual = fila.front();
+                    fila.pop();
+                    std::cout << g[atual].name << " ";
+
+                    for (auto e : boost::make_iterator_range(out_edges(atual, g))) {
+                        auto w = target(e, g);
+                        if (!visitado[index[w]]) {
+                            visitado[index[w]] = 1;
+                            tin[index[w]] = ++tempo;
+                            fila.push(w);
+                        }
+                    }
+
+                    tout[index[atual]] = ++tempo;
+                }
+                std::cout << "\n";
+            }
+
+            std::cout << "\nTempos (descoberta/finalização):\n";
+            for (auto v : boost::make_iterator_range(vertices(g))) {
+                std::cout << std::left << std::setw(12) << g[v].name
+                        << " d=" << tin[index[v]] << " f=" << tout[index[v]] << "\n";
+            }
         }
+    } else { // AQUI SERÁ IMPLEMENTADA A BFS DE GUIGOLA-----------------------------------------------------------------------------------------------------------
+
+        
+
+
     }
+
+
+
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,13 @@
-readme do projeto
+# Descrição do Projeto
+
+Implementação de algoritmos de busca por Profundidade (DFS) e Largura (BFS) em grafos interpretados à partir de arquivos '.dot'.
+
+# Integrantes do Projeto
+
+Carlos Alberto de Lima Neto
+Lutércio Jackson Guimarães Filho
+Luiz Guilherme Carvalho Viana
+
+# Compilação do Projeto
+
+abc
